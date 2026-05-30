3cc7fe7b0def0e8a0489bbfdd7667d7506363b41
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2026-05-30 22:41:07 +0530

    fix(deployment):added logs to fix error

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log('RAW ENV:', JSON.stringify({
+    PORT: process.env.PORT,
+    DATABASE_URL: !!process.env.DATABASE_URL,
+    REDIS_URL: !!process.env.REDIS_URL,
+  }));
+
   app.enableCors({
     origin:["https://suntek-ai-round-2.vercel.app", "http://localhost:3001"],
     credentials: true,
@@ -13,7 +19,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true,transform:true}))
 
-  await app.listen(env.port ?? 3000,"0.0.0.0",()=>{console.log("Server running")});
+  await app.listen(process.env.PORT ?? 3000,"0.0.0.0",()=>{console.log("Server running")});
 }
 
 bootstrap();
