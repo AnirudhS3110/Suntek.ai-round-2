f01a1b295cf50c8e09560f1b8adb9a2876e49246
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2026-05-30 22:25:14 +0530

    fix(backend):bnding server to 0.0.0.0

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,13 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin:"https://suntek-ai-round-2.vercel.app",
+    origin:["https://suntek-ai-round-2.vercel.app", "http://localhost:3001"],
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true,transform:true}))
 
-  await app.listen(env.port ?? 3000);
+  await app.listen(env.port ?? 3000,"0.0.0.0");
 }
 
 bootstrap();
