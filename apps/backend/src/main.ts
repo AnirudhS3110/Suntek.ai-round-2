bcc9db42c7f4ed8d087988eeb80d7c7f5613ea59
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2026-05-30 22:36:05 +0530

    fix(deployment):added logs to fix error

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({whitelist:true,forbidNonWhitelisted:true,transform:true}))
 
-  await app.listen(env.port ?? 3000,"0.0.0.0");
+  await app.listen(env.port ?? 3000,"0.0.0.0",()=>{console.log("Server running")});
 }
 
 bootstrap();
